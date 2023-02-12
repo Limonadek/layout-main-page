e09adf9b0bdb68b9da5fb7ff7632409440778935
Limonadek <den-stepanov-03@mail.ru>
2023-02-12 23:24:09 +0300

    add more image

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import './index.css';
+import logo from './image/logo.png';
+import vectorText from './image/vector-text.png';
+import vector from './image/vector.png';
+
+const headerImage = document.querySelector('.header--logo');
+headerImage.src = logo;
+
+const footerVector = document.querySelector('.footer-logo-container--icon');
+footerVector.src = vector;
+
+const footerVectorText = document.querySelector('.footer-logo-container--text');
+footerVectorText.src = vectorText;
