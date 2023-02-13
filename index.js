d2949aa0894696a12f1cf4d5cbb3727096bfc2ec
Limonadek <den-stepanov-03@mail.ru>
2023-02-13 20:02:33 +0300

    finish footer

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
+import './index.html';
 import './index.css';
-import logo from './image/logo.png';
-import vectorText from './image/vector-text.png';
-import vector from './image/vector.png';
-
-const headerImage = document.querySelector('.header--logo');
-headerImage.src = logo;
-
-const footerVector = document.querySelector('.footer-logo-container--icon');
-footerVector.src = vector;
-
-const footerVectorText = document.querySelector('.footer-logo-container--text');
-footerVectorText.src = vectorText;
